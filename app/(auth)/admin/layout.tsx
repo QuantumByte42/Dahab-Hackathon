4b0e0575bca16dc2f3ee6cb081b5d036a8cc5766
Yacoub Altayeh (Home) <yacoub.altayeh@icloud.com>
2025-06-02 11:18:50 +0300

    toast config

--- app/(auth)/admin/layout.tsx
+++ app/(auth)/admin/layout.tsx
@@ -78,7 +78,7 @@ export default function RootLayout({
           pauseOnFocusLoss
           draggable
           pauseOnHover
-          theme="dark"
+          theme="light"
         />
       </body>
     </html>
